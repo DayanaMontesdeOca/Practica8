316fc2b664434e6f547da50add12f22db59c7591
DayanaMontesdeOca <fi.montesdeoca.unam@gmail.com>
2025-10-29 14:25:46 -0600

    Create For.c

---
+++ For.c
@@ -0,0 +1,32 @@
+/*Rivera Montes de Oca Dayana Monserrat
+Practica 08 Lbaoratorio EJERCICIO 4 DEL PDF*/
+
+//Escriba un programa en C que pida dos números enteros al usuario, y determine cuál es
+//mayor, el primero o el segundo. Modifíquelo para considerar también la posibilidad de
+//que sean iguales.
+
+#include <stdio.h>
+int main() 
+  {
+    int n1, n2, i;
+    for (i= 1; i<= 2; i++) 
+      {
+        printf("Ingrese un numero %d: ", i);
+        scanf("%d", &n1);
+        if (i == 1) 
+          {
+            n2=n1;
+          } 
+        else 
+          {
+            if (n1>n2)
+                printf("El segundo número (%d) es mayor que el primero (%d)\n", n1, n2);
+            else 
+                if (n1<n2)
+                  printf("El primer número (%d) es mayor que el segundo (%d)\n", n2, n1);
+                else
+                  printf("Ambos números son iguales (%d)\n", n1);
+          }
+      }
+    return 0;
+  }
