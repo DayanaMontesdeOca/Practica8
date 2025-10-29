ea1a9f3ded1410be1b87f2d537661f6deb83649b
DayanaMontesdeOca <fi.montesdeoca.unam@gmail.com>
2025-10-29 14:39:52 -0600

    Create While.c

---
+++ While.c
@@ -0,0 +1,29 @@
+/*Rivera Montes de Oca Dayana Monserrat
+Lboratorio Practica 08, EJERCICIO 4 DEL EXAMEN*/
+
+//Programa con una condición con while, que almacene un arreglo 
+//undimensional de los primeros 10 números pares y que imprima el arreglo.
+
+#include <stdio.h>
+
+int main() 
+  {
+      int Pares[10];   
+      int i=0;   
+      int num=2;  
+      while (i<10) 
+        {
+            Pares[i]=num;
+            num+=2;
+            i++;
+        }
+      i = 0;
+      printf("Los primeros 10 números pares son:\n");
+      while (i<10) 
+        {
+          printf("%d ", Pares[i]);
+          i++;
+        }
+      printf("\n");
+      return 0;
+  }
